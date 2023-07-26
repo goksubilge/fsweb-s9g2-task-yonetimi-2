390a0df2b6a17209f770189ef544e15403b2e122
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-07-26 09:01:23 +0300

    add locale tr

--- src/Task.js
+++ src/Task.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { differenceInDays, formatDistance } from "date-fns";
+import { differenceInDays, formatDistanceToNow } from "date-fns";
 import { tr } from "date-fns/locale";
 
 const Task = ({ taskObj, onComplete }) => {
   const diffInDays = differenceInDays(new Date(taskObj.deadline), new Date());
+  const distanceToNow = formatDistanceToNow(new Date(taskObj.deadline), {
+    locale: tr,
+  });
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
       <div className="deadline">
         son teslim:{" "}
         <span>
-          {taskObj.deadline} - {diffInDays}
+          {taskObj.deadline} / {diffInDays} / {distanceToNow}
         </span>
       </div>
       <p>{taskObj.description}</p>
