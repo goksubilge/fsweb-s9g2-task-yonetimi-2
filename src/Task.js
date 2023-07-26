33a0841a0f766061b1001f8535e8aa84dab846e2
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-07-26 08:46:28 +0300

    modify dates to today

--- src/Task.js
+++ src/Task.js
@@ -1,18 +1,25 @@
 import React from "react";
+import { DifferenceInDays, formatDistance } from "date-fns";
+import { tr } from "date-fns/locale";
 
 const Task = ({ taskObj, onComplete }) => {
-
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">
+        son teslim: <span>{taskObj.deadline}</span>
+      </div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className="pill" key={p}>
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>
+      )}
     </div>
   );
 };
--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ export const initialTasks = [
     description:
       "Bugün öğrendiklerimizden hangilerini projede kullanabiliriz, hangi kısımları mevcut bilgimizle yapabiliriz?",
     people: ["ömer", "emre"],
-    deadline: "2023-01-23",
+    deadline: "2023-07-23",
     status: "yapıldı",
   },
   {
@@ -16,7 +16,7 @@ export const initialTasks = [
     description:
       "Projede neler yapmamız gerekiyor? Aklımıza gelen her adımı yazalım.",
     people: ["ömer"],
-    deadline: "2023-01-25",
+    deadline: "2023-07-25",
     status: "yapılacak",
   },
   {
@@ -25,7 +25,7 @@ export const initialTasks = [
     description:
       "Hangi adımdan başlasak daha iyi olur? Yapılacakları doğru sıraya koymaya çalışmak da bize fikir verebilir.",
     people: ["emre"],
-    deadline: "2023-01-27",
+    deadline: "2023-07-27",
     status: "yapılacak",
   },
   {
